2830dfc9fbe73a0748caa8f77af2b03a658d354c
AMATEURTOSS <amateur.toss@gmail.com>
2024-07-14 17:57:50 +0900

    update(): lsp에 tailwind 추가

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -2,7 +2,7 @@ local lspconfig = require('lspconfig')
 
 lspconfig.tsserver.setup{}
 
-local servers = { 'tsserver' }
+local servers = { 'tsserver', 'tailwindcss' }
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     -- on_attach = my_custom_on_attach,
