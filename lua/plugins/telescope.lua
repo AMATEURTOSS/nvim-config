10543f787fef55e77e3c71bb6649e00a13ba8840
yochoi <amateur.toss@gmail.com>
2025-12-15 11:47:05 +0900

    update: telescope 추가

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "파일 찾기" })
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "텍스트 찾기" })
+    vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "열린 파일 전환" })
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "도움말 검색" })
+  end,
+}
